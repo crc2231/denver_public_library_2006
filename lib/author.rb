6c8398df95b0f38410d5251d20020cd4c803dfe7
Corey Carter <crc2231@gmail.com>
2020-07-22 13:16:54 -0600

    Finalize Write method/test

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,28 @@
 require './lib/book'
 
-class Author
+class Author < Book
+  attr_reader :first_name, :last_name, :books
 
-def initialize(details)
-  @first_name = details[:first_name]
-  @last_name = details[:last_name]
-  @books = []
-end
+  def initialize(details)
+    @first_name = details[:first_name]
+    @last_name = details[:last_name]
+    @books = []
+  end
+
+  def name
+    @first_name + " " + @last_name
+  end
+
+  def write(book_name, book_publication_date)
+    details = {}
+    details[:author_first_name] = self.first_name
+    details[:author_last_name] = self.last_name
+    details[:title] = book_name
+    details[:publication_date] = book_publication_date
+    book = Book.new(details)
+    books << book
+    book
+  end
 
 
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -11,6 +11,28 @@ class AuthorTest < Minitest::Test
   end
 
   def test_it_has_attributes
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    assert_equal "Charlotte Bronte", charlotte_bronte.name
+
+    assert_equal [], charlotte_bronte.books
+  end
+
+  def test_write_makes_books
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    assert_instance_of Book, jane_eyre
+
+    assert_equal "Jane Eyre", jane_eyre.title
+  end
+
+  def test_write_adds_books
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    charlotte_bronte.write("Villette", "1853")
+
+    assert_equal 2, charlotte_bronte.books.count
   end
 
 end
