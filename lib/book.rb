2711f93d1fc1d981830824307c3aa3819e1935d4
Corey Carter <crc2231@gmail.com>
2020-07-22 12:38:28 -0600

    Initialize class

---
+++ lib/book.rb
@@ -0,0 +1,11 @@
+class Book
+
+  def initialize(details)
+    @author_first_name = details[:author_first_name]
+    @author_last_name = details[:author_last_name]
+    @title = details[:title]
+    @publication_date = details[:publication_date]
+  end
+
+
+end
