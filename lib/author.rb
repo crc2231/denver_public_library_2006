1c6949d0e36598ae753559acd50ec9d6e2495e62
Corey Carter <crc2231@gmail.com>
2020-07-22 12:50:35 -0600

    Create Author class/test with attributes

---
+++ lib/author.rb
@@ -0,0 +1,12 @@
+require './lib/book'
+
+class Author
+
+def initialize(details)
+  @first_name = details[:first_name]
+  @last_name = details[:last_name]
+  @books = []
+end
+
+
+end
---
+++ test/author_test.rb
@@ -0,0 +1,16 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/author"
+
+class AuthorTest < Minitest::Test
+
+  def test_it_exists
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    assert_instance_of Author, charlotte_bronte
+  end
+
+  def test_it_has_attributes
+  end
+
+end
